b6917985310b643ee013fd1391a4d55295a3e8e7
javaveryhot <javaveryhot@protonmail.ch>
2023-05-17 21:12:32 +0200

    Initial plugin setup

---
+++ src/main/java/org/noxet/noxetserver/NoxetServer.java
@@ -0,0 +1,14 @@
+package org.noxet.noxetserver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NoxetServer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
